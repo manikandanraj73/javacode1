81bad45ebf93c169f916b7a86144ccb4d1b3f6a4
manikandanraj73 <manikandan60167@gmail.com>
2026-03-06 08:22:39 -0800

    Update Reversing.java
    add a code for reverse a string

--- javacode/Reversing.java
+++ javacode/Reversing.java
@@ -4,6 +4,14 @@ class Reversing {
     public static void main(String[] args){
     base.reversNumber();
     }
+    public static void revrseString(){
+    System.out.print("Enter a Word: ");
+    String word=scanner.nextLine();
+    for(int i=word.length()-1;i>=0;i--){
+        System.out.print(word.charAt(i));
+    }
+    System.out.println("");
+}
     public static void reversNumber(){
         System.out.print("Enter Number: ");
         int num=scanner.nextInt();
